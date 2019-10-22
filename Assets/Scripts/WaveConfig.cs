3e9facd31a7ee1ea31775c5d4858bf6803c8ab8d
Will Lacey <WillLacey@Will-Laceys-MacBook-Pro-4.local>
2019-10-22 12:17:16 -0500

    Created WaveConfig Script and Wave Scriptable Object

---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject getEnemyPrefab()
+    {
+        return enemyPrefab;
+    }
+
+    public GameObject getPathPrefab()
+    {
+        return pathPrefab;
+    }
+
+    public float getTimeBetweenSpawns()
+    {
+        return timeBetweenSpawns;
+    }
+
+    public float getSpawnRandomFactor()
+    {
+        return spawnRandomFactor;
+    }
+
+    public int getNumberOfEnemies()
+    {
+        return numberOfEnemies;
+    }
+
+    public float getMoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
